c4b449d27e666cc7262c443e0a33135439267f18
BRodriguez90 <bero14@knights.ucf.edu>
2017-03-26 19:23:31 -0400

    function wrapped breedfind

--- scripts/api2.js
+++ scripts/api2.js
@@ -2,30 +2,25 @@ $(function(){
 
    //var k = "02D17E46482089F8721CF5579632E2E3";
    var petkey = "7b6195f6609c531192c20efa3751c3d4";
-   var petscret = "2a8d945c5e24848c8b467d2427047db9";
-   var petApi = 'http://api.petfinder.com/breed.list?format=json&key=';
-   $('#animal').click(function(){
+   //var petscret = "2a8d945c5e24848c8b467d2427047db9";
+   var petBreedApi = 'http://api.petfinder.com/breed.list?format=json&key=';
+   var petFindApi = 'http://api.petfinder.com/pet.find?format=json&key=';
+
+   $('select#animal').on('mouseout',function(){ //
+      getBreed();
+   }); //end of click event function
+
+   function getBreed(){
       var user = $("select").val();
       $('#breed').children().remove();
-      //var steamApi = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+k+"&jsoncallback=?";
-      $.getJSON( petApi + petkey + '&animal=' + user + '&callback=?',
+      $.getJSON( petBreedApi + petkey + '&animal=' + user + '&callback=?',
          function(data){
             console.log(data.petfinder.breeds.breed);
             $.each(data.petfinder.breeds.breed, function(i,field){
                $('#breed').append("<option value=\""+field.$t+"\">"+field.$t+"</option>");
                //$("#profile").append(field.$t + " ");
-            });
-      });
+            }); // end of each
+      }); // end of getJson
+   } // end of getBreed function
 
-      /*$.ajax({
-         url:'http://api.petfinder.com/breed.list?format=json&key='+petkey+'&animal='+user+'&jsoncallback=?',
-         type:'GET',
-         dataType:'json',
-         success:function(results){
-            alert(results);
-         }
-      });*/
-
-
-   });
-});
+}); // jQuery closing tag
