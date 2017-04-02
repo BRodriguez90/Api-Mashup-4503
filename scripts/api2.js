fb65b6747e8ff712d6b1826d09e972498cee1ede
BRodriguez90 <bero14@knights.ucf.edu>
2017-04-02 12:32:28 -0400

    added lastupdate

--- scripts/api2.js
+++ scripts/api2.js
@@ -1,7 +1,7 @@
 var pet_info = [];
 var shLatLng = [];
 $(function() {
-
+$  ('.carousel').carousel();
     //var k = "02D17E46482089F8721CF5579632E2E3";
     var petkey = "7b6195f6609c531192c20efa3751c3d4";
     var petBreedApi = 'http://api.petfinder.com/breed.list?format=json&key=';
@@ -44,8 +44,11 @@ $(function() {
                     $('.row').append('<p id="noresults">No animal matches found.</p>');
                 } else {
                     $.each(result.petfinder.pets.pet, function(i, field) {
-                        var get_shelter = field.shelterId.$t; // get shelter ids
-                        var get_info = {
+                        var get_shelter = field.shelterId.$t;           // get shelter ids
+                        var lastUpdate = field.lastUpdate.$t;           // get last updated info
+                        var dateparse = String(Date.parse(lastUpdate)); // convert updated info object into a string and parse to readable format
+                        var extdate = dateparse.slice(0,15);            //slice unwanted time info from date
+                        var get_info = {                                //new object to be pushed into global pet_info array
                             name: field.name.$t,
                             age: field.age.$t,
                             description: field.description.$t,
@@ -53,49 +56,38 @@ $(function() {
                             picture: field.media.photos.photo[2].$t
                         };
                         pet_info.push(get_info);
-                        //console.log(getShelter);
+                        var pet_card_info =                  // html for the pet cards
+                           '<div class="col-md-4">' +
+                              '<div class="pet_cards">' +
+                                 '<img class="adopt_pet" src=' + field.media.photos.photo[3].$t + '>' +
+                                 '<div class="card_info">' +
+                                    '<p>Name: ' + field.name.$t + '</p>' +
+                                    '<p>Sex: ' + field.sex.$t + '</p>' +
+                                    '<p>Location: ' + field.contact.city.$t + ', ' + field.contact.state.$t + '</p>' +
+                                    '<p id="date">Last Updated: ' + extdate + '</p>' +
+                                 '</div>' +
+                              '</div>' +
+                           '</div>';
                         $('<input/>').attr('class', 'shelter_id').attr('type', 'hidden').attr('value', get_shelter).appendTo('.row');
-                        //$('<img/>').attr('src', field.media.photos.photo[1].$t).appendTo('#profile');
-                        $('.row').append("<div class='col-md-4'><div class='pet_cards'><img class='adopt_pet' src=" + field.media.photos.photo[3].$t + "> <div class='card_info'><p>Name: " + field.name.$t + "</p><p>Sex: " + field.sex.$t + "</p>" + "<p>Location: " + field.contact.city.$t + ", " + field.contact.state.$t + "</p></div></div></div>");
+                        $('.row').append(pet_card_info);
+                        //$('.row').append("<div class='col-md-4'><div class='pet_cards'><img class='adopt_pet' src=" + field.media.photos.photo[3].$t + "> <div class='card_info'><p>Name: " + field.name.$t + "</p><p>Sex: " + field.sex.$t + "</p>" + "<p>Location: " + field.contact.city.$t + ", " + field.contact.state.$t + "</p></div></div></div>");
                         //console.log(field.media.photos.photo[1].$t);
 
-                        $.getJSON(petShelterApi + petkey + '&id=' + get_shelter + '&callback=?',
+                        $.getJSON(petShelterApi + petkey + '&id=' + get_shelter + '&callback=?', // Get shelter ids query string
                             function(data2) {
-                                //console.log(data2);
                                 var latlng = {
                                     lat: data2.petfinder.shelter.latitude.$t,
                                     lng: data2.petfinder.shelter.longitude.$t,
                                     shelter: data2.petfinder.shelter.name.$t
                                 };
-                                //var lng = {data2.petfinder.shelter.longitude.$t};
                                 shLatLng.push(latlng);
                             });
-                    }); // end each loop
-                } // end else
-            }); // end getJSON
-    } //end of findPet function
+                    });       // end each loop
+                }            // end else
+            });             // end getJSON
+    }                      //end of findPet function
 
-    /*function testing(){
-       for (var i = 0; i < shLatLng.length; i++) {
-          //console.log(shLatLng[i].name);
-          console.log(shLatLng[i].lat);
-          console.log(shLatLng[i].lng);
-          console.log(shLatLng[i].shelter);
-       }
-       for (var i = 0; i < pet_info.length; i++) {
-          console.log(pet_info[i].name);
-       }
-    }*/
-
-    $('#testsearch').on('click', function(e) {
-        e.preventDefault();
-        //findShelter();
-        // addMarker();
-        testing();
-        // console.log(shLatLng);
-        //shLatLng = [];
-    });
-    $('#search').on('click', function(e) { //search for pets search button
+    $('#search').on('click', function(e) {   //search for pets search button
         e.preventDefault();
         $('.row').children().remove();
         findPet();
