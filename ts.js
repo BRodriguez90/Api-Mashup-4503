e3c5b957daee8d4fb568724fef942811a2d62b73
BRodriguez90 <bero14@knights.ucf.edu>
2017-04-05 16:57:45 -0400

    added button smoothscroll

--- ts.js
+++ ts.js
@@ -16,3 +16,17 @@
          '</div>'+ // back end
       '</div>' + // container end
    '</div>'; // col end
+
+
+
+   '<div class="col-md-4">' +
+      '<div class="pet_cards">' +
+         '<img class="adopt_pet" src=' + field.media.photos.photo[3].$t + '>' +
+         '<div class="card_info">' +
+            '<p>Name: ' + field.name.$t + '</p>' +
+            '<p>Sex: ' + field.sex.$t + '</p>' +
+            '<i class="fa fa-paw" aria-hidden="true"></i><p>Location: ' + field.contact.city.$t + ', ' + field.contact.state.$t + '</p>' +
+            '<i class="fa fa-calendar" aria-hidden="true"></i><p id="date">Last Updated: ' + extdate + '</p>' +
+         '</div>' +
+      '</div>' +
+   '</div>';
