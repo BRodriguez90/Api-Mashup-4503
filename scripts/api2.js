bfdebacf7449ac3f8f78522069901064e1eccd59
BRodriguez90 <bero14@knights.ucf.edu>
2018-02-19 23:53:45 -0500

    minor es6 changes

--- scripts/api2.js
+++ scripts/api2.js
@@ -1,7 +1,15 @@
 let pet_info = [];
 let shLatLng = [];
 $(function() {
-$  ('.carousel').carousel();
+    let img_array = ["images/beagle-pup.jpg","images/dog-pup.jpeg"];
+    const hero_img = document.getElementById("puppy_hero");
+
+
+      //for(let i = 0; i < img_array.length; i++){
+       //setInterval( () => hero_img.src = img_array[0], 10000);
+       //setInterval( () => hero_img.src = img_array[1], 20000);
+      //}
+
     const petkey = "7b6195f6609c531192c20efa3751c3d4";
     const petBreedApi = 'http://api.petfinder.com/breed.list?format=json&key=';
     const petFindApi = 'http://api.petfinder.com/pet.find?format=json&key=';
@@ -13,24 +21,17 @@ $  ('.carousel').carousel();
         $('.breed_select').fadeIn();
     }); //end of click event function
 
-    getBreed = () => {
-        let fetchanimal = $("#animal").val();
+   const getBreed = () => {
+        const fetchanimal = '&animal=' + $("#animal").val();
+        const callback = '&callback=?';
         $('#breed').children().remove();
-        $.getJSON(petBreedApi + petkey + '&animal=' + fetchanimal + '&callback=?',
+        $.getJSON(petBreedApi + petkey + fetchanimal + callback,
             function(data) {
-              let breed = data.petfinder.breeds.breed; 
-              for (let i = 0; i < breed.length; i++) {
-                let breed_name = breed[i].$t;
-                $('#breed').append(`<option value="${breed_name}"> ${breed_name}</option>`);
-              }
-              /*breed.forEach((type,i) => {
-                let {$t} = type;
-                 $('#breed').append(`<option value="${$t}"> ${$t} </option>`);
-              });*/
-               // $.each(breed, function(i, field) {
-                    //$('#breed').append("<option value=\"" + field.$t + "\">" + field.$t + "</option>");
-                   //  $('#breed').append(`<option value="${field.$t}"> ${field.$t} </option>`);
-              // });     // end of each
+              const {breed} = data.petfinder.breeds;
+                $.each(breed, function(i, field) {
+                    let breed_name = field.$t;
+                    $('#breed').append(`<option value="${breed_name}"> ${breed_name} </option>`);
+               });     // end of each
             });        // end of getJson
     }                 // end of getBreed function
 
@@ -40,25 +41,26 @@ $  ('.carousel').carousel();
     $(document).ajaxComplete(function() {
         $('#rolling').hide();
     });
-    findPet = () => {
-        let fetchanimal = $('#animal').val();                  // grab animal type
-        let fetchbreed = $('#breed').val();                    // grab animal breed
-        let fetchgender = $('#gender').val();                  // grab sex of animal
-        let fetchlocation = $('#location').val();              // grab location to search
+   const findPet = () => {
+        const fetchanimal = '&animal=' + $("#animal").val();           // grab animal type
+        const fetchbreed = '&breed=' + $('#breed').val();                    // grab animal breed
+        const fetchgender = '&sex=' + $('#gender').val();                  // grab sex of animal
+        const fetchlocation = '&location=' + $('#location').val();              // grab location to search
+        const callback = '&callback=?';
 
-        $.getJSON(petFindApi + petkey + '&animal=' + fetchanimal + '&breed=' + fetchbreed + '&sex=' + fetchgender + '&count=6' + '&location=' + fetchlocation + /*'&distance=' + fetchdistance +*/ '&callback=?',
+        $.getJSON(petFindApi + petkey + fetchanimal + fetchbreed + fetchgender + '&count=6' + fetchlocation + callback,
             (result) => {
                 console.log(result);
                 if ($.isEmptyObject(result.petfinder.pets)) { // if no results are found.
                     $('#row_append').append('<p id="noresults">No animal matches found.</p>');
                 } else {
                     $.each(result.petfinder.pets.pet, (i, field) => {
-                        let get_shelter = field.shelterId.$t;           // get shelter ids
-                        let lastUpdate = field.lastUpdate.$t;           // get last updated info
-                        let dateparse = String(Date.parse(lastUpdate)); // convert updated info object into a string and parse to readable format
-                        let extdate = dateparse.slice(0,15);            //slice unwanted time info from date
-                   
-                        let get_info = {                                //new object to be pushed into global pet_info array
+                        const get_shelter = field.shelterId.$t;           // get shelter ids
+                        const lastUpdate = field.lastUpdate.$t;           // get last updated info
+                        const dateparse = String(Date.parse(lastUpdate)); // convert updated info object into a string and parse to readable format
+                        const extdate = dateparse.slice(0,15);            //slice unwanted time info from date
+
+                        const get_info = {                                //new object to be pushed into global pet_info array
                             name: field.name.$t,
                             age: field.age.$t,
                             description: field.description.$t,
@@ -69,63 +71,55 @@ $  ('.carousel').carousel();
                             picture: field.media.photos.photo[2].$t
                         };
 
-                       noInfoGiven = () => {                            //To check if there is no information for the given object fields 
+                     const noInfoGiven = () => {                            //To check if there is no information for the given object fields
                            get_info.description === undefined ? get_info.description = "No description available." : get_info.description;
                            get_info.phone === undefined ? get_info.phone = "N/A" : get_info.phone;
                            get_info.email === undefined ? get_info.email = "N/A" : get_info.email;
                        }
                         noInfoGiven();
 
-                        pet_info.push(get_info); 
+                        pet_info.push(get_info);
+
                         field.sex.$t === "M" ? field.sex.$t = "Male" : "M";
                         field.sex.$t === "F" ? field.sex.$t = "Female" : "F";
-                             /*if(field.sex.$t === "M"){
 
-                             field.sex.$t = "Male";
-
-                          }else if(field.sex.$t === "F"){
-
-                             field.sex.$t = "Female";
-                          }*/
                           //Changed to template string
-                            const pet_card_info =                  // html for the pet cards 
+                            const pet_card_info =                  // html for the pet cards
                            `<div class="col-md-4">
                               <div class="pet_cards">
-                                 <img class="adopt_pet" src= "${field.media.photos.photo[3].$t}"  data-position="80" data-blur="5" data-focus="10" data-falloff="20" data-direction="x" >
+                                 <img class="adopt_pet" src= "${field.media.photos.photo[3].$t}"  data-position="80" data-blur="5" data-focus="10" data-falloff="20" data-direction="x">
                                  <div class="card_info">
-                                    <p>Name: ${get_info.name} </p> 
-                                    <p><i class="fa fa-venus-mars" aria-hidden="true"></i> ${field.sex.$t} <span id="mix">/ Mix:  ${field.mix.$t} </p> 
+                                    <p>Name: ${get_info.name} </p>
+                                    <p><i class="fa fa-venus-mars" aria-hidden="true"></i>: ${field.sex.$t} <span id="mix">/ Mixed Breed:  ${field.mix.$t} </p>
                                     <i class="fa fa-paw" aria-hidden="true"></i><p>Location: ${field.contact.city.$t}, ${field.contact.state.$t} </p>
                                     <i class="fa fa-calendar" aria-hidden="true"></i><p id="date">Last Updated:  ${extdate} </p>
-                                 </div> 
-                              </div> 
+                                 </div>
+                              </div>
                            </div>`;
                         $('<input/>').attr('class', 'shelter_id').attr('type', 'hidden').attr('value', get_shelter).appendTo('.row');
                         $('#row_append').append(pet_card_info);
-                        //$('.row').append("<div class='col-md-4'><div class='pet_cards'><img class='adopt_pet' src=" + field.media.photos.photo[3].$t + "> <div class='card_info'><p>Name: " + field.name.$t + "</p><p>Sex: " + field.sex.$t + "</p>" + "<p>Location: " + field.contact.city.$t + ", " + field.contact.state.$t + "</p></div></div></div>");
-                        //console.log(field.media.photos.photo[1].$t);
-
+                    
                         $.getJSON(petShelterApi + petkey + '&id=' + get_shelter + '&callback=?', // Get shelter ids query string
                             (data2) => {
                               const noLatLng = () => {
-                                    console.log(data2.petfinder.header.status.code.$t);   
-                                    data2.petfinder.header.status.code.$t === "300" ? console.log("Not Available") : console.log("Available");             
+                                    console.log(data2.petfinder.header.status.code.$t);
+                                    data2.petfinder.header.status.code.$t === "300" ? console.log("Not Available") : console.log("Available");
                                     if(data2.petfinder.header.status.code.$t === "300"){
+
                                      let noLat =  data2.petfinder.shelter.latitude.$t = "0";
                                      let noLng =  data2.petfinder.shelter.longitude.$t = "0";
                                      let noShelt = data2.petfinder.shelter.name.$t = "Not given.";
                                     }
                                 }
                                 noLatLng();
-
                                let latlng = {
                                     lat: data2.petfinder.shelter.latitude.$t,
                                     lng: data2.petfinder.shelter.longitude.$t,
                                     shelter: data2.petfinder.shelter.name.$t
-                                };   
-                               
+                                };
+
                                 shLatLng.push(latlng);
-                              
+
                             });
                     });       // end each loop
                 }            // end else
@@ -133,13 +127,13 @@ $  ('.carousel').carousel();
     }                      //end of findPet function
 
     $('#search').on('click', function(e) {   //search for pets search button
-        e.preventDefault();
+        e.stopPropagation()
         $('#row_append').children().remove();
         $('.album').show();
        // new google.maps.event.trigger(map, "resize");
        pet_info = [];
        shLatLng = [];
-        findPet();
+      findPet();
     });
 
 
